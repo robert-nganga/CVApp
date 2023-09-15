496de93ce2ecf9f4a25d9dc926b48a88cabe7f41
robert-nganga <robertnganga220@gmail.com>
2023-09-15 11:11:00 +0300

    Add edit details form

--- app/src/main/java/com/nganga/robert/cvapp/EditScreen.kt
+++ app/src/main/java/com/nganga/robert/cvapp/EditScreen.kt
@@ -1,6 +1,24 @@
 package com.nganga.robert.cvapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nganga.robert.cvapp.components.DetailsFormField
 
 
 @Composable
@@ -9,5 +27,56 @@ fun EditScreen(
 ){
     val personalDetails = detailsViewModel.personalDetails
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp)
+    ) {
+        EditScreenAppBar(title = "Update Personal Details")
+        Spacer(modifier = Modifier.height(16.dp))
+        DetailsFormField(
+            value = personalDetails.names,
+            onValueChanged = detailsViewModel::updateName
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        DetailsFormField(
+            value = personalDetails.slackUserName,
+            onValueChanged = detailsViewModel::updateSlackUserName
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        DetailsFormField(
+            value = personalDetails.githubHandle,
+            onValueChanged = detailsViewModel::updateGithubHandle
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        DetailsFormField(
+            value = personalDetails.bio,
+            onValueChanged = detailsViewModel::updateBio
+        )
 
+    }
+}
+
+@Composable
+fun EditScreenAppBar(
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "back icon"
+        )
+        Spacer(modifier = Modifier.width(30.dp))
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
---
+++ app/src/main/java/com/nganga/robert/cvapp/components/DetailsFormField.kt
@@ -0,0 +1,32 @@
+package com.nganga.robert.cvapp.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsFormField(
+    value: String,
+    modifier: Modifier = Modifier,
+    label: String = "",
+    onValueChanged: (String)->Unit
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        modifier = modifier
+            .fillMaxWidth(),
+        label = {
+            Text(text = label)
+        },
+        maxLines = 1,
+        shape = RoundedCornerShape(8.dp)
+    )
+}
